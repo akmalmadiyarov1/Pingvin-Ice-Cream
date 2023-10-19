73ac29d602907c123d9fc09a0cef84d84f47f703
akmalmadiyarov1 <122540934+akmalmadiyarov1@users.noreply.github.com>
2023-10-19 15:20:18 +0500

    strated

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Main from './layout/components/main/Main'
+import Header from './layout/components/header/Header'
+import Footer from './layout/components/footer/Footer'
+
+
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Main/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/layout/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from '../../../../public/images/Logo.png'
+function Header() {
+  return (
+    <div>
+        <div className="header">
+            <div className="header-logo">
+                <img src={logo} alt="" />
+            </div>
+            <div className="header-links">
+
+            </div>
+            <div className="header-button">
+
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
